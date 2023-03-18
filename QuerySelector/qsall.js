8f2b108fc4ec7b1b5fd57e745902c5b8b8ab925d
Vardhan Agarwal <72724034+Bishop1835@users.noreply.github.com>
2023-03-18 19:05:53 +0530

    querselector js file

---
+++ QuerySelector/qsall.js
@@ -0,0 +1,32 @@
+// query selector
+// let header = document.querySelector('#main-header');
+// header.getElementsByClassName.borderBottom = 'solid 4px black';
+
+// let input = document.querySelector('input');
+// input.value = 'hello'
+
+// let submit = document.querySelector('input[type="submit"]');
+// submit.value='SEND';
+
+// let item = document.querySelector('.list-group-item');
+// item.style.color ='red';
+
+// let lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color = 'blue';
+
+// let secondItem = document.querySelector('.list-group-item:nth-child(3)');
+// secondItem.style.color = 'coral';
+
+// for class use . and for id use # 
+// queryselectorall
+
+// let title =document.querySelectorAll('.title');
+// console.log(title);
+// title[0].textContent = 'hello';
+
+
+// let odd = document.querySelectorAll('li:nth-child(odd)');
+
+// for ( let i=0; i<odd.length; i++){
+//     odd[i].style.backgroundColor = "#ccc";
+// }
