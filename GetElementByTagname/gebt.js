f2f5900db99c7ec2c466f7c2d8419c42b938f4be
Vardhan Agarwal <72724034+Bishop1835@users.noreply.github.com>
2023-03-18 19:00:29 +0530

    geetbytag js

---
+++ GetElementByTagname/gebt.js
@@ -0,0 +1,78 @@
+// examine the document object
+
+// console.log("heloo");
+// console.log("document");
+// console.log(document.head);
+// console.dir(document);
+// document.title=123;
+// console.log(document.all);
+// console.log(document.all[10]);
+
+// getelementbyid
+
+// // console.log(document.getElementById('header-title'));
+// // let headerTitle = document.getElementById('header-title');
+// // let header = document.getElementById('main-header');
+// let main = document.getElementById('add-item');
+// console.log(main);
+// // // console.log(headerTitle);
+// // // headerTitle.textContent = 'hello'; 
+// // // headerTitle.innerText = 'goodbye';
+
+// // // console.log(headerTitle.textContent); // 123 in the console part
+// // // console.log(headerTitle.innerText); //123 will not be in console
+// // // console.log(headerTitle);
+
+// // headerTitle.innerHTML = '<h3>Hello</h3>'; //innerhtml dont change the DOm lement it puts inside the DOm element
+// // header.style.borderBottom = ' solid 3px #000';
+// main.style.fontWeight ='bold';
+// main.style.color= 'green';
+
+
+
+
+// // getelementbyclass
+
+// let items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]); 
+
+// items[1].textContent='ITEMS';
+// items[1].style.backgroundColor = 'yellow';
+
+// //gets an error
+// //items.style.backgroundColor = 'yellow';
+
+// for(let i=0; i< items.length; i++){
+//     items[i].style.backgroundColor ='yellow';
+// }
+
+// items[2].style.backgroundColor = 'green';
+
+// for(let i=0; i<items.length; i++){
+//     items[i].style.fontWeight ='bold';
+//     items[i].style.color ='green'
+// }
+
+// // getelementbytagname
+
+// let li  = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li); 
+
+// li[1].textContent='ITEMS';
+// li[1].style.backgroundColor = 'yellow';
+
+// // gets an error
+// // li.style.backgroundColor = 'yellow';
+
+// for(let i=0; i< items.length; i++){
+//     li[i].style.backgroundColor ='yellow';
+// }
+
+// li[2].style.backgroundColor = 'green';
+
+// for(let i=0; i<li.length; i++){
+//     li[i].style.fontWeight ='bold';
+//     li[i].style.color ='green'
+// }
