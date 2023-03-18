e7bb5a3ed3e1cbae7f1ca51460797851ef3c016a
Vardhan Agarwal <72724034+Bishop1835@users.noreply.github.com>
2023-03-18 18:57:41 +0530

    getbyid jsfile

---
+++ GetElementById/gebi.js
@@ -0,0 +1,30 @@
+// examine the document object
+
+// console.log("heloo");
+// console.log("document");
+// console.log(document.head);
+// console.dir(document);
+// document.title=123;
+// console.log(document.all);
+// console.log(document.all[10]);
+
+// getelementbyid
+
+// // console.log(document.getElementById('header-title'));
+// // let headerTitle = document.getElementById('header-title');
+// // let header = document.getElementById('main-header');
+// let main = document.getElementById('add-item');
+// console.log(main);
+// // // console.log(headerTitle);
+// // // headerTitle.textContent = 'hello'; 
+// // // headerTitle.innerText = 'goodbye';
+
+// // // console.log(headerTitle.textContent); // 123 in the console part
+// // // console.log(headerTitle.innerText); //123 will not be in console
+// // // console.log(headerTitle);
+
+// // headerTitle.innerHTML = '<h3>Hello</h3>'; //innerhtml dont change the DOm lement it puts inside the DOm element
+// // header.style.borderBottom = ' solid 3px #000';
+// main.style.fontWeight ='bold';
+// main.style.color= 'green';
+
